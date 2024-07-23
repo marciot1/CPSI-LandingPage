0fe0bf6e4fbe64e509f99620437915542ddc528d
Marcio Targino Do Nascimento <marcio.t@escolar.ifrn.edu.br>
2024-07-23 10:55:09 -0300

    CPSI-LandingPage

--- stylejavascript.js
+++ stylejavascript.js
@@ -1,10 +1,11 @@
 let contador = 1;
 
+const newLocal = document.getRootNode('slide ' + contador).checked = AudioNode;
 setInterval( function(){
     document.getElementById('slide' + contador).checked = true;
     contador++;
 
     if(counter > 5 ) {
-        contador = 1;
+        contador = 5;
     }
 }, 3000 );
